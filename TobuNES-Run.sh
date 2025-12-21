1527243e70328b0daca45a8c81052f5afce37df0
Chad Burrow <stevenchadburrow@gmail.com>
2025-12-20 20:10:19 -0600

    Add files via upload

--- TobuNES-Run.sh
+++ TobuNES-Run.sh
@@ -1,4 +1,10 @@
+
+#./NesGameDev-Composer.o TobuNES-Song.bmp TobuNES-Song.asm ;
+
 ./asm6/asm6.o TobuNES-Code.asm TobuNES-ProgramROM.bin ;
 ./NesGameDev-Converter.o TobuNES-PatternTable0.bmp TobuNES-PatternTable1.bmp TobuNES-CharacterROM.bin ;
 ./NesGameDev-Combiner.o TobuNES-ProgramROM.bin TobuNES-CharacterROM.bin TOBUNES.NES ;
+zip TOBUNES.NES.zip TOBUNES.NES ;
+
+#./Mesen TOBUNES.NES.zip ;
 ./PICnes.o TOBUNES.NES ;
